2df8b4fb13109158e34dbd16d6d30beed1d23ea9
Danil Vinogradov <vinogradovspbu@gmail.com>
2022-07-04 15:39:19 +0300

    feat: add User model validations

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -1,14 +1,23 @@
 package ru.yandex.practicum.filmorate.model;
 
 import lombok.Data;
+import org.springframework.lang.NonNull;
+import org.springframework.lang.Nullable;
 
-import java.util.Date;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.PastOrPresent;
+import javax.validation.constraints.Pattern;
+import java.time.LocalDate;
+import java.util.Objects;
 
 /**
  * Модель пользователя.
  */
 @Data
 public final class User {
+
+    private static long counter = 1;
 
     /**
      * Уникальный целочисленный идентификатор.
@@ -18,20 +27,47 @@ public final class User {
     /**
      * Электронная почта.
      */
+    @Email
     private final String email;
 
     /**
      * Логин.
      */
+    @NonNull
+    @NotBlank
+    @Pattern(regexp = "^[a-zA-Z0-1]+$")
     private final String login;
 
     /**
      * Имя пользователя (для отображения).
      */
+    @Nullable
     private final String name;
 
     /**
      * День рождения.
      */
-    private final Date birthday;
+    @NonNull
+    @PastOrPresent
+    private final LocalDate birthday;
+
+    public User(final @Nullable Long id,
+                final @NonNull String email,
+                final @NonNull String login,
+                final @Nullable String name,
+                final @NonNull LocalDate birthday) {
+
+        this.id = Objects.requireNonNullElseGet(id, () -> counter++);
+
+        this.email = email;
+        this.login = login;
+
+        if (name == null || name.isBlank()) {
+            this.name = login;
+        } else {
+            this.name = name;
+        }
+
+        this.birthday = birthday;
+    }
 }
