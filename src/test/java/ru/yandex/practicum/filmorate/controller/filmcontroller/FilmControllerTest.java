b922af7c0b93587766a64a077ddd566c3a2f83e2
Danil Vinogradov <vinogradovspbu@gmail.com>
2022-07-05 17:00:28 +0300

    test(refactor): configure webClient & delete useless assertions

--- src/test/java/ru/yandex/practicum/filmorate/controller/filmcontroller/FilmControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/filmcontroller/FilmControllerTest.java
@@ -2,9 +2,13 @@ package ru.yandex.practicum.filmorate.controller.filmcontroller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import ru.yandex.practicum.filmorate.controller.filmcontroller.model.FilmModelNoId;
 import ru.yandex.practicum.filmorate.controller.filmcontroller.model.FilmModelNoIdWFloatPointDuration;
@@ -13,24 +17,41 @@ import ru.yandex.practicum.filmorate.model.Film;
 import util.ControllerTestHelper;
 
 import java.io.IOException;
+
+import org.springframework.http.HttpHeaders;
+
 import java.time.LocalDate;
-import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 /**
  * Тесты для {@link FilmController} и валидации данных в {@link Film}.
+ *
+ * @see Film
+ * @see FilmController
  */
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 final class FilmControllerTest {
 
-    private static final String URI = "/films";
-
     private static final ObjectMapper objectMapper = ControllerTestHelper.getObjectMapper();
 
-    private static final WebTestClient webClient = WebTestClient.bindToController(FilmController.class).build();
+    private static WebTestClient webClient;
+
+    @BeforeEach
+    void beforeEach() {
+        webClient = WebTestClient.bindToController(FilmController.class)
+                .configureClient()
+                .baseUrl("/films")
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .defaultHeader(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
+                .build();
+    }
+
+    @AfterEach
+    void afterEach() {
+        webClient = null;
+    }
 
     /**
      * Корректно добавляется один фильм с валидными данными.
@@ -39,25 +60,15 @@ final class FilmControllerTest {
      */
     @Test
     void shouldCorrectlyAddAndRetrieveFilms() throws IOException {
-        final FilmModelNoId expectedFilm = new FilmModelNoId("NAME_" + Math.random(),
-                "DESCRIPTION_" + Math.random(),
-                LocalDate.of(1995, 5, 4),
-                ((int) (Math.random() * 100)));
+        final FilmModelNoId expectedFilm = new FilmModelNoId("Shakeita Begin",
+                "Dose term games jazz factors cartridge prayer, entry notebook.",
+                LocalDate.of(1995, 5, 4), 271);
 
-        final byte[] responseBody = webClient.post()
-                .uri(URI)
+        webClient.post()
                 .bodyValue(objectMapper.writeValueAsString(expectedFilm))
-                .header("Content-Type", "application/json")
                 .exchange()
-                .expectStatus().is2xxSuccessful()
-                .expectBody().returnResult().getResponseBody();
-
-        final Film actualDeserializedFilm = objectMapper.readValue(responseBody, Film.class);
-
-        assertEquals(expectedFilm.getName(), actualDeserializedFilm.getName());
-        assertEquals(expectedFilm.getDescription(), actualDeserializedFilm.getDescription());
-        assertEquals(expectedFilm.getReleaseDate(), actualDeserializedFilm.getReleaseDate());
-        assertEquals(expectedFilm.getDuration(), actualDeserializedFilm.getDuration());
+                .expectStatus().isOk()
+                .expectBody(FilmModelNoId.class).isEqualTo(expectedFilm);
     }
 
     /**
@@ -67,33 +78,28 @@ final class FilmControllerTest {
      */
     @Test
     void shouldCorrectlyRetrieveFilms() throws IOException {
-        final FilmModelNoId film1 = new FilmModelNoId("film1_name", "film2_description",
+        final FilmModelNoId film1 = new FilmModelNoId("Quentina Nyberg Dierdre",
+                "Name realty teaches unexpected.",
                 LocalDate.of(2001, 12, 4), 14);
-        final FilmModelNoId film2 = new FilmModelNoId("film2_name", "film2_description",
+        final FilmModelNoId film2 = new FilmModelNoId("Shaneaka Tisha Vangorder Chaise Spector",
+                "Holiday bible causing sig oxford hunt kilometers.",
                 LocalDate.of(1993, 4, 17), 121);
 
         final List<FilmModelNoId> filmsToAdd = List.of(film1, film2);
 
-        for (FilmModelNoId film : filmsToAdd) {
+        for (final FilmModelNoId film : filmsToAdd) {
             webClient.post()
-                    .uri(URI)
-                    .header("Content-Type", "application/json")
                     .bodyValue(objectMapper.writeValueAsString(film))
                     .exchange()
                     .expectStatus().isOk();
         }
 
-        final byte[] responseBody = webClient.get()
-                .uri(URI)
+        webClient.get()
                 .exchange()
                 .expectStatus().isOk()
-                .expectBody().returnResult().getResponseBody();
-
-        final List<FilmModelNoId> actualDeserializedFilmList = Arrays.asList(objectMapper.readValue(responseBody,
-                FilmModelNoId[].class));
-
-        assertEquals(filmsToAdd.size(), actualDeserializedFilmList.size());
-        assertTrue(actualDeserializedFilmList.containsAll(filmsToAdd));
+                .expectBody(new ParameterizedTypeReference<List<FilmModelNoId>>() {
+                })
+                .isEqualTo(filmsToAdd);
     }
 
     /**
@@ -107,28 +113,24 @@ final class FilmControllerTest {
      */
     @Test
     void shouldReturn400WhenDescriptionLengthIsMoreThan200() throws JsonProcessingException {
+        // 200 знаков description можно
         final String description200 = "a".repeat(200);
-        final FilmModelNoId film1 = new FilmModelNoId("film1_name", description200,
+        final FilmModelNoId film1 = new FilmModelNoId("Graylon Hilliard", description200,
                 LocalDate.of(2001, 12, 4), 14);
 
-        // 200 знаков description можно
         webClient.post()
-                .uri(URI)
                 .bodyValue(objectMapper.writeValueAsString(film1))
-                .header("Content-Type", "application/json")
                 .exchange()
                 .expectStatus().isOk()
-                .expectBody()
-                .jsonPath("$.description").isNotEmpty();
-
-        final FilmModelNoId film2 = new FilmModelNoId("film2_name", "a".repeat(201),
-                LocalDate.of(2001, 12, 4), 14);
+                .expectBody(FilmModelNoId.class)
+                .isEqualTo(film1);
 
         // 201 или больше уже нельзя
+        final FilmModelNoId film2 = new FilmModelNoId("Dakota Claassen", "a".repeat(201),
+                LocalDate.of(1901, 11, 19), 94);
+
         webClient.post()
-                .uri(URI)
                 .bodyValue(objectMapper.writeValueAsString(film2))
-                .header("Content-Type", "application/json")
                 .exchange()
                 .expectStatus().isBadRequest()
                 .expectBody().isEmpty();
@@ -146,29 +148,23 @@ final class FilmControllerTest {
      */
     @Test
     void shouldReturn400WhenNameIsNullOrBlank() throws JsonProcessingException {
-        // name is null
-        final FilmModelNoId film1 = new FilmModelNoId(null, "film1_description",
+        // name is null (invalid)
+        final FilmModelNoId film1 = new FilmModelNoId(null,
+                "Autos mean cardiff camps buffalo management usd, enquiry amd monroe.",
                 LocalDate.of(2001, 12, 4), 14);
 
-        // name is blank
-        final FilmModelNoId film2 = new FilmModelNoId("  \n", "film2_description",
+        // name is blank (invalid)
+        final FilmModelNoId film2 = new FilmModelNoId("  \n",
+                "Occur merit toll europe customers funds the, memorabilia moss yourself vernon demonstrated.",
                 LocalDate.of(1993, 4, 17), 121);
 
-        webClient.post()
-                .uri(URI)
-                .bodyValue(objectMapper.writeValueAsString(film1))
-                .header("Content-Type", "application/json")
-                .exchange()
-                .expectStatus().isBadRequest()
-                .expectBody().isEmpty();
-
-        webClient.post()
-                .uri(URI)
-                .bodyValue(objectMapper.writeValueAsString(film2))
-                .header("Content-Type", "application/json")
-                .exchange()
-                .expectStatus().isBadRequest()
-                .expectBody().isEmpty();
+        for (final var film : List.of(film1, film2)) {
+            webClient.post()
+                    .bodyValue(objectMapper.writeValueAsString(film))
+                    .exchange()
+                    .expectStatus().isBadRequest()
+                    .expectBody().isEmpty();
+        }
     }
 
     /**
@@ -184,67 +180,48 @@ final class FilmControllerTest {
      */
     @Test
     void shouldReturn400WhenDurationIsNotPositive() throws JsonProcessingException {
-        // null
+        // null (invalid)
         final FilmModelNoId film1 = new FilmModelNoId("film1_description", "film1_description",
                 LocalDate.of(2001, 12, 4), null);
 
-        // отрицательное значение
+        // отрицательное значение (invalid)
         final FilmModelNoId film2 = new FilmModelNoId("film2_description", "film2_description",
                 LocalDate.of(2001, 12, 4), -10);
 
-        // zero
+        // zero (invalid)
         final FilmModelNoId film3 = new FilmModelNoId("film3_description", "film3_description",
                 LocalDate.of(2001, 12, 4), 0);
 
-        // duration в виде вещественного числа
+        // duration в виде вещественного числа (valid)
         final FilmModelNoIdWFloatPointDuration film4 = new FilmModelNoIdWFloatPointDuration("film4_description",
                 "film4_description", LocalDate.of(2001, 12, 4), 60.7);
 
-        // пограничный случай, duration == 1
+        // пограничный случай, duration == 1 (valid)
         final FilmModelNoId film5 = new FilmModelNoId("film5_description", "film5_description",
                 LocalDate.of(2001, 12, 4), 1);
 
-        webClient.post()
-                .uri(URI)
-                .bodyValue(objectMapper.writeValueAsString(film1))
-                .header("Content-Type", "application/json")
-                .exchange()
-                .expectStatus().isBadRequest()
-                .expectBody().isEmpty();
+        // invalid data
+        for (final var film : List.of(film1, film2, film3)) {
+            webClient.post()
+                    .bodyValue(objectMapper.writeValueAsString(film))
+                    .exchange()
+                    .expectStatus().isBadRequest()
+                    .expectBody().isEmpty();
+        }
 
         webClient.post()
-                .uri(URI)
-                .bodyValue(objectMapper.writeValueAsString(film2))
-                .header("Content-Type", "application/json")
-                .exchange()
-                .expectStatus().isBadRequest()
-                .expectBody().isEmpty();
-
-        webClient.post()
-                .uri(URI)
-                .bodyValue(objectMapper.writeValueAsString(film3))
-                .header("Content-Type", "application/json")
-                .exchange()
-                .expectStatus().isBadRequest()
-                .expectBody().isEmpty();
-
-        webClient.post()
-                .uri(URI)
                 .bodyValue(objectMapper.writeValueAsString(film4))
-                .header("Content-Type", "application/json")
                 .exchange()
                 .expectStatus().isOk()
                 .expectBody()
-                .jsonPath("$.duration").isNumber();
+                .jsonPath("$.duration").isEqualTo(film4.getDuration().intValue());
 
         webClient.post()
-                .uri(URI)
                 .bodyValue(objectMapper.writeValueAsString(film5))
-                .header("Content-Type", "application/json")
                 .exchange()
                 .expectStatus().isOk()
                 .expectBody()
-                .jsonPath("$.duration").isNumber();
+                .jsonPath("$.duration").isEqualTo(film5.getDuration());
     }
 
     /**
@@ -258,44 +235,38 @@ final class FilmControllerTest {
      * @see FilmController#addFilm(Film)
      */
     @Test
-    void shouldReturn400WhenReleaseDateIsBefore28_12_1895() throws JsonProcessingException {
-        // пограничный случай, 28 декабря 1895 года (валидное значение)
-        final FilmModelNoId film1 = new FilmModelNoId("film1_description", "film1_description",
+    void shouldReturn400WhenReleaseDateIsBefore28_12_1895() throws IOException {
+        // пограничный случай, 28 декабря 1895 года (valid)
+        final FilmModelNoId film1 = new FilmModelNoId("Claudine Mclain"
+                , "Dealt always mostly actress technical contacts relate.",
                 LocalDate.of(1895, 12, 28), 120);
 
-        // раньше 28 декабря 1895
-        final FilmModelNoId film2 = new FilmModelNoId("film2_description", "film2_description",
-                LocalDate.of(1895, 12, 27), 120);
+        // дата в будущем (valid)
+        final FilmModelNoId film2 = new FilmModelNoId("Yentel Muhammad",
+                "Fireplace gratis requesting necklace compact franklin entry, trips jonathan logan proteins.",
+                LocalDate.now().plusDays(10), 220);
 
-        // дата в будущем
-        final FilmModelNoId film3 = new FilmModelNoId("film3_description", "film3_description",
-                LocalDate.now().plusDays(10), 120);
+        // раньше 28 декабря 1895 (invalid)
+        final FilmModelNoId film3 = new FilmModelNoId("Caryn Rogalski",
+                "Cry suites humanitarian spouse televisions combinations thomson, core narrow compatibility.",
+                LocalDate.of(1895, 12, 27), 320);
 
+        // valid models
+        for (final FilmModelNoId film : List.of(film1, film2)) {
+            webClient.post()
+                    .bodyValue(objectMapper.writeValueAsString(film))
+                    .exchange()
+                    .expectStatus().isOk()
+                    .expectBody(FilmModelNoId.class)
+                    .isEqualTo(film);
+        }
+
+        // invalid value
         webClient.post()
-                .uri(URI)
-                .bodyValue(objectMapper.writeValueAsString(film1))
-                .header("Content-Type", "application/json")
-                .exchange()
-                .expectStatus().isOk()
-                .expectBody()
-                .jsonPath("$.releaseDate").exists();
-
-        webClient.post()
-                .uri(URI)
-                .bodyValue(objectMapper.writeValueAsString(film2))
-                .header("Content-Type", "application/json")
+                .bodyValue(objectMapper.writeValueAsString(film3))
                 .exchange()
                 .expectStatus().isBadRequest()
                 .expectBody().isEmpty();
-
-        webClient.post()
-                .uri(URI)
-                .bodyValue(objectMapper.writeValueAsString(film3))
-                .header("Content-Type", "application/json")
-                .exchange()
-                .expectStatus().isOk()
-                .expectBody()
-                .jsonPath("$.releaseDate").exists();
     }
 
     /**
@@ -306,40 +277,31 @@ final class FilmControllerTest {
     @Test
     void shouldReturn400WhenUpdatingNonExistentFilm() throws IOException {
         // будет добавлен
-        final FilmModelNoId film1 = new FilmModelNoId("film1_description", "film1_description",
+        final FilmModelNoId film1 = new FilmModelNoId("Daphane Storey",
+                "Navigator removing spider counted francis mistakes hi, leaf wifi cubic fioricet val haven.",
                 LocalDate.of(1995, 12, 28), 120);
 
-        final byte[] film1ResponseBody = webClient.post()
-                .uri(URI)
+        // добавляем фильм
+        final FilmModelWId createdFilm = webClient.post()
                 .bodyValue(objectMapper.writeValueAsString(film1))
-                .header("Content-Type", "application/json")
                 .exchange()
                 .expectStatus().isOk()
-                .expectBody()
-                .jsonPath("$.id").isNumber()
+                .expectBody(FilmModelWId.class)
                 .returnResult().getResponseBody();
 
-        final long film1Id = objectMapper.readValue(film1ResponseBody, FilmModelWId.class).getId();
-        final FilmModelWId film1Updated = new FilmModelWId(film1Id, "film1_description_updated",
-                "film1_description", LocalDate.of(1995, 12, 28), 120);
+        assertNotNull(createdFilm);
+        assertNotNull(createdFilm.getId());
 
+        // не будет добавлен, попробуем сразу обновить
+        // генерируем несуществующий идентификатор (так как добавлен только 1 фильм, то операция существующего id
+        // с любым числом, кроме нейтрального элемента генерирует не существующий id)
+        final FilmModelWId film2 = new FilmModelWId(createdFilm.getId() + 121, "Willem Vogler Amon",
+                "Trusts girls identifier door exactly traditional compute, correction fonts hunting person.",
+                LocalDate.of(1965, 5, 1), 255);
+
+        // обновление фильма с несуществующим идентификатором вызывает ошибку
         webClient.put()
-                .uri(URI)
-                .bodyValue(objectMapper.writeValueAsString(film1Updated))
-                .header("Content-Type", "application/json")
-                .exchange()
-                .expectStatus().isOk()
-                .expectBody()
-                .jsonPath("$.name").isNotEmpty();
-
-        // не будет добавлен
-        final FilmModelWId film2 = new FilmModelWId(film1Id + 121, "film2_description", "film2_description",
-                LocalDate.of(1995, 12, 28), 120);
-
-        webClient.put()
-                .uri(URI)
                 .bodyValue(objectMapper.writeValueAsString(film2))
-                .header("Content-Type", "application/json")
                 .exchange()
                 .expectStatus().is5xxServerError()
                 .expectBody().isEmpty();
